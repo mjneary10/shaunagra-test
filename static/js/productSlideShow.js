906fedbfaad36aaa7e28aede7298c4de94726db8
mjneary10 <mjneary10@gmail.com>
2025-03-06 12:43:16 +0000

    Updated javascript file

--- static/js/productSlideShow.js
+++ static/js/productSlideShow.js
@@ -15,7 +15,6 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
@@ -26,7 +25,6 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
 
